607062a916cbc6374dbd52dc45061fd483ae0434
Rebecca Tamar Fried <btf4645@cs.rit.edu>
2018-10-09 20:55:17 -0400

    I updated the function classfication that properly determines the correct message/integer based on the ascii value

--- tokenize.c
+++ tokenize.c
@@ -6,7 +6,7 @@
 #include "classes.h"
 
 char states[25][25] = {0};
-int initial[25][25] = {0};
+char initial[25][25] = {0};
 int count = 0;
 int width = 0;
 int accept = 0;
@@ -24,101 +24,91 @@ void  matrix(int count, int width){
     }
 }
 
-char classification(int count, int width){
-  char class;
-  for(int i = 0; i < count; i++)
-    for(int j = 0; j < width; j++){
-      if(initial[i][j] == '0'){
-	class = CC_WS;
-      }
-      else if (initial[i][j] == '1'){
-	class = CC_NEWLINE;
-      }
-      else if (initial[i][j] == '2'){
-	class = CC_ALPHA;
-      }
-      else if (initial[i][j] == '3'){
-	class = CC_DIG_0;
-      }
-      else if (initial[i][j] == '4'){
-	class = CC_DIG_1_7;
-      }
-      else if (initial[i][j] == '5'){
-	class = CC_DIG_8_9;
-      }
-      else if (initial[i][j] == '6'){
-	class = CC_CHAR_SLASH;
-      }
-      else if (initial[i][j] == '7'){
-	class = CC_CHAR_STAR;
-      }
-      else if (initial[i][j] == '8'){
-	class = CC_ARITH_OP;
-      }
-      else if (initial[i][j] == '9'){
-	class = CC_OTHER;
-      }
-      else if (initial[i][j] == '10'){
-	class = CC_EOF;
-      }
-      else if (initial[i][j] == '11'){
-	class = CC_ERROR;
-      }
-      else if (initial[i][j] == '12'){
-	class = N_CC;
-      }
-    }
-  return class;
+int classification(char class){
+  if(class == 32 || class == 9){
+    return CC_WS;
+  }
+  else if (class == 13){
+    return CC_NEWLINE;
+  }
+  else if ((class >= 65 && class <= 90) && class == 95){
+    return CC_ALPHA;
+  }
+  else if (class == 48){
+    return CC_DIG_0;
+  }
+  else if (class >= 49 && class <= 55){
+    return CC_DIG_1_7;
+  }
+  else if (class == 56 || class == 57){
+    return CC_DIG_8_9;
+  }
+  else if (class == 47){
+    return CC_CHAR_SLASH;
+  }
+  else if (class == 42){
+    return CC_CHAR_STAR;
+  }
+  else if (class == 37 || class == 43 || class == 45){
+    return CC_ARITH_OP;
+  }
+  else if ((class >= 0 && class <= 12) || (class >= 14 && class <= 36) || (class >= 38 && class <= 41) ||
+	   (class >= 50 && class <= 54) || (class >= 58 && class <= 64) || (class >= 91 && class <= 94) ||
+	   (class >= 96 && class <= 127)){
+    return CC_OTHER;
+  }
+  else if (class == EOF){
+    return CC_EOF;
+  }
+  else{
+    return CC_ERROR;
+  }
 }
 
+
 void scanner(FILE *fp){
-  count = 4;
+  count = 0;
   int lineLen = 0;
-  size_t n = 0;
-  ssize_t num = -1;
-  char* valuesPtr;
+  //size_t n = 0;
+  //ssize_t num = -1;
+  //char* valuesPtr;
   char* ptr, buf[256];
   
-  while((ptr = getline(&buf, &n, fp)) > 0){
+  while((ptr = fgets(buf, 256, fp)) != NULL){
     if (count == 0){
       char word[256];
       sscanf(ptr, "%s %d", word, &statesv);
       printf("%s %d\n", word, statesv);
     }
-    if (count == 1) {
+    else if (count == 1) {
       char word[256];
       sscanf(ptr, "%s %d", word, &start);
       printf("%s %d\n", word, start);
     }
-    if (count == 2) {
+    else if (count == 2) {
       char word[256];
       sscanf(ptr, "%s %d", word, &accept);
       printf("%s %d\n", word, accept);
     }
-    
-
-
-    lineLen = strlen(buf);
-    char values[lineLen / 2];
-    for(int i = 0; i < lineLen; i++){
-      values[i/2] = buf[i];
+    else{
+      char StringCurrent[266];
+      strcpy(StringCurrent, ptr);
+      char* pch;
+      pch = strtok(StringCurrent, ",.-");
+      while(pch != NULL){
+	printf("%s\n", pch);
+	pch = strtok(NULL, ",.-");
+      }
+      lineLen = strlen(ptr);
+      for(int i = 0; i < lineLen; i++){
+	initial[count][i] = pch;
+      }
     }
-    //    valuesPtr = strdup(values);
-    // initial[valuesPtr][lineLen] = valuesPtr;
-    
-    //initial[count][i] = buf[i];
+    count++;    
   }
 
-    
-    count++;
+  
 
-    //  width = lineLen;
-
-    // for(int i = 0; i < 100; i++){
-    // for (int j = 0; j < 100; j++){
-    // printf("%d", initial[i][j]);
-    // }
-    //}
 }
 
 int main(int argc, char* argv[]){
