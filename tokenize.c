975cd2a612299fa83d0e3dfc991cb68438e5e788
Rebecca Tamar Fried <btf4645@cs.rit.edu>
2018-10-12 17:27:12 -0400

    I got almost everything working, all of the matrix function is done!

--- tokenize.c
+++ tokenize.c
@@ -1,3 +1,5 @@
+#define _DEFAULT_SOURCE
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
@@ -5,7 +7,8 @@
 #include <string.h>
 #include "classes.h"
 
-char *initial[75][12] = {0};
+
+char *initial[75][12];
 int count = 0;
 int accept = 0;
 int start = 0;
@@ -13,22 +16,72 @@ int statesv = 0;
 int classcharacter;
 int statenumber;
 char dors = '\0';
-
+int classification(char a);
 
 
 void  matrix(){
-  
+  statenumber = start;
+  printf("%d " , statenumber);
+  int a;
+  int num;
+  char* agoodpointer;
+  int counter = 0;
+  a = getchar();
+  while(a != EOF){
+    num = classification(a);
+    char* aspot;
+    aspot = initial[statenumber][num];
+    sscanf(aspot, "%d%c", &statenumber, &dors);
+    if(dors == 's'){
+      printf("%d ", statenumber);
+    }
+    if(statenumber == 99){
+      printf("%d ", statenumber);
+      printf("rejected");
+      printf("\n");
+      a = getchar();
+      num = classification(a);
+      while(num != 0){
+	a = getchar();
+	num = classification(a);
+      }
+      statenumber = start;
+      printf("%d ", statenumber);
+    }
+    else if(statenumber == accept){
+      printf("%d ", statenumber);
+      printf("recognized ");
+      printf("\n");
+      statenumber = start;
+      printf("%d ", statenumber);
+    }
+    a = getchar();
+  }
+  num = classification(a);
+
+  char* donez =  initial[statenumber][num];
+  sscanf(donez, "%d%c", &statenumber, &dors);
+  if(dors == 's'){
+    printf("%d ", statenumber);
+  }
+  else if (statenumber == 99){
+    printf("%d rejected\n", statenumber);
+  }
+  else if(statenumber == accept){
+    printf("%d EOF\n", statenumber);
+  }
 }
+    
 
 
 int classification(char class){
   if(class == 32 || class == 9){
     return CC_WS;
   }
-  else if (class == 13){
+  else if (class == 10 || class == 13){
     return CC_NEWLINE;
   }
-  else if ((class >= 65 && class <= 90) && class == 95){
+  else if ((class >= 65 && class <= 90) || class == 95 || (class >= 97 && class <= 122)){
     return CC_ALPHA;
   }
   else if (class == 48){
@@ -49,9 +102,8 @@ int classification(char class){
   else if (class == 37 || class == 43 || class == 45){
     return CC_ARITH_OP;
   }
-  else if ((class >= 0 && class <= 12) || (class >= 14 && class <= 36) || (class >= 38 && class <= 41) ||
-	   (class >= 50 && class <= 54) || (class >= 58 && class <= 64) || (class >= 91 && class <= 94) ||
-	   (class >= 96 && class < 127)){
+  else if ((class >= 0 && class <= 8) || (class == 11) || (class == 12) || (class >= 14 && class <= 31) || (class >= 33 && class <= 36)
+	   || (class >= 38 && class <= 41) || (class == 44) || (class == 46) || (class >= 58 && class <= 64) || (class >= 91 && class <= 94) || (class == 96) || (class >= 123 && class < 127)){
     return CC_OTHER;
   }
   else if (class == EOF){
@@ -70,7 +122,6 @@ void scanner(FILE *fp){
   statenumber = -1;
   dors = '\0';
   int zates;
-  int* p = (int*)malloc(2 * sizeof(int));
 
   
   while((ptr = fgets(buf, 256, fp)) != NULL){
@@ -103,7 +154,7 @@ void scanner(FILE *fp){
 	if(atoken != NULL) {
 	  //printf("%s\n", atoken);
 	  sscanf(atoken, "%d/%d%c", &classcharacter, &zates, &dors);
-	  char anotherarray[3] = "";
+	  char *anotherarray = (char*) malloc(3);
 	  sprintf(anotherarray, "%d%c", zates, dors);
 	  //printf("[%d][%d] = %d%c\n", statenumber,classcharacter,zates,dors); 
 	  initial[statenumber][classcharacter] = anotherarray;
@@ -113,12 +164,15 @@ void scanner(FILE *fp){
     }
     count++;    
   }
+  printf("Scanning using the following matrix\n");
   printf("%s", "      0     1     2     3     4     5     6     7     8     9     10     11\n");
   for(int i = 0; i < statenumber; i++){
     printf("%d ", i);
     for(int j = 0; j < 12; j++){
       if(initial[i][j] == NULL){
-	initial[i][j] = "99d";
+	char* agoodone = (char*) malloc(3);
+	agoodone = strdup("99d");
+	initial[i][j] = agoodone;
       }
       printf("  %4s" , initial[i][j]);
       }
@@ -129,6 +183,9 @@ void scanner(FILE *fp){
 int main(int argc, char* argv[]){
   FILE *fp;
 
+  if (argc == 1){
+    printf("usage: /tokenize tmfile \n");
+  }
   fp = fopen(argv[1], "r");
   if (fp == NULL){
     perror(argv[1]);
@@ -136,5 +193,14 @@ int main(int argc, char* argv[]){
   }
   
   scanner(fp);
-
+  fclose(fp);
+  
+  matrix();
+  
+  for(int i = 0; i < statenumber; i++){
+    for(int j = 0; j < 12; j++){
+      free(initial[i][j]);
+    }
+  }
+  return 0;
 }
